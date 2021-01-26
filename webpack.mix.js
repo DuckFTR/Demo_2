d0930827ae92bc02bda4beea4c0e7796aab68d76
DuckFTR <ducker.ru@gmail.com>
2021-01-26 19:14:39 +0300

    повторная загрузка

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,7 +1,12 @@
-// webpack.mix.js
+
 let mix = require('laravel-mix');
 
 mix.sass('src/scss/styles.scss', 'dist/css');
 
 mix.copy('src/index.html', 'dist/index.html');
 mix.copyDirectory('src/images', 'dist/images');
+
+//дополнение по url
+mix.sass('src/app.scss', 'dist').options( {
+    processCssUrls:false
+    });
